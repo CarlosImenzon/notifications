7927ee1d5bb9cba6d50a3d0043ce023b2077111e
Juanse1998 <Juansesosa98@gmail.com>
2020-05-10 21:32:41 -0300

    Borra 0004_create_documents

--- db/migrations/0003_create_documents.rb
+++ db/migrations/0003_create_documents.rb
@@ -1,8 +1,8 @@
 Sequel.migration do 
 	up do 
 		create_table(:documents) do
-			#primary_key :id
-			String 		:title,	null: false
+			primary_key :id
+			String 		:title,
 			String		:topic, null: false
 			#String  	:tag, null: false
 		end
