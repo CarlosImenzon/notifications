3ba198ee2200eb95cc7f7e10149aa9410d47b983
Juanse1998 <Juansesosa98@gmail.com>
2020-05-13 17:28:45 -0300

    Agrega validaciones

--- models/document.rb
+++ models/document.rb
@@ -1,2 +1,9 @@
 class Document < Sequel::Model
+	plugin :validation_helpers
+    def validate
+    	super
+        	validates_presence [:title, :topic, :id]
+  		end
+		many_to_many  :users
+		set_primary_key :id
 end
--- models/user.rb
+++ models/user.rb
@@ -1,2 +1,11 @@
 class User < Sequel::Model
+	plugin :validation_helpers
+	def validate
+		super
+		validates_presence [:name, :username, :email, :password]
+		validates_format /\A.*@.*\..*\z/, :email, message: 'Error'
+		errors.add(:name, 'cannot be empty') if !name || name.empty?
+	end
+	many_to_many :documents
+	set_primary_key :id
 end
