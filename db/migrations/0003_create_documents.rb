87932b46914e1283566534d70c353181d2ec9ee2
Juanse1998 <Juansesosa98@gmail.com>
2020-05-13 22:36:31 -0300

    Corrección Error

--- db/migrations/0003_create_documents.rb
+++ db/migrations/0003_create_documents.rb
@@ -2,7 +2,7 @@ Sequel.migration do
 	up do 
 		create_table(:documents) do
 			primary_key :id
-			String 		:title,
+			String 		:title
 			String		:topic, null: false
 			#String  	:tag, null: false
 		end
