c226994363e170178b1beba560b44a5cc7c0042d
Juanse1998 <Juansesosa98@gmail.com>
2020-06-26 04:13:03 -0300

    Validación corregida

--- models/user.rb
+++ models/user.rb
@@ -5,7 +5,7 @@ class User < Sequel::Model
 		validates_presence [:name, :username, :email, :password]
 		validates_unique [:username]
 		validates_unique [:email]
-		validates_format /\A.*@.*\..*\z/, :email
+		validates_format /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :email
 	end
 	many_to_many :documents
 	one_to_many :relations
