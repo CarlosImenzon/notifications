f5a4abbad1f879c06b4405766ea7d07c2d173662
juanse <juansesosa98@gmail.com>
2020-11-19 14:28:22 -0300

    Refactorizacion Document terminada [Finishes #175629993]

--- services/documentService.rb
+++ services/documentService.rb
@@ -1,26 +1,22 @@
-require './models/user.rb'
+
 require './models/document.rb'
+require './exceptions/ValidationModelError.rb'
 
 include FileUtils::Verbose
 
-
 class Document_Service
 
-
-  def self.new_document(title, topic, file)
-    document = Document.new(title: title, topic:topic, file: file)
-     if document.valid? && !@filename.nil? # Si el documento es valido se guarda
+  def self.new_document(title, topic, file, tagusers)
+      document = Document.new(title: title, topic: topic, file: file)
+      if document.valid? && !file.nil? # Si el documento es valido se guarda
         document.save
-        tagusers = params['multi_select']
         tagusers.each do |u| # Etiqueta de usuarios
         Relation.new(document_id: document.id, user_id: u.to_i).save
       end
         cp(file.path, "public/#{document.id}#{document.file}")
         File.chmod(0o777, "public/#{document.id}#{document.file}")
-        redirect '/'
       else
-        @error = 'Error al cargar documento, verifique los campos'
-        @users = User.order(:username)
+        raise ValidationModelError.new("Error al cargar el documento",1)
       end
   end
 
